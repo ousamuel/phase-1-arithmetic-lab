c808561dd78f22a19be2db2ee36e982446e7ecf0
Samuel <samuelou510@gmail.com>
2023-06-12 14:38:16 -0400

    arithmetic lab finished

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 4;
+let num4 = 16;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random())
+// console.log(Math.ceil(Math.random() + Math.random()*10));
+
+let mod = num4 / num3;
+let max = Math.max(1, 2, 20);
